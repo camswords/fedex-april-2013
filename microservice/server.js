ca5a31a34a586acdad361c8822c979ceae2709a7
Cam Swords <cam.swords@gmail.com>
2013-04-18 17:36:49 +1000

    the server uses registered components instead of directly calling them

--- microservice/server.js
+++ microservice/server.js
@@ -1,16 +1,16 @@
 var express = require('express');
+var _ = require('underscore');
 var networkHeader = require('./lib/network_header_handlers');
 var networkFooter = require('./lib/network_footer_handlers');
 
+
+var components = [networkHeader, networkFooter];
 var app = express();
 
 app.use(function(error, request, response, next){
     console.error(err.stack);
-    res.send(500, 'Something broke!');
+    res.send(500, 'whoopsi daisy! something has gone and broked.');
 });
-
-networkHeader.addRoutes(app);
-networkFooter.addRoutes(app);
 
 app.get('/', function(request, response) {
 	var links = [{ rel: 'components', href: 'http://localhost:3000/components', type: 'application/json' }];
@@ -19,10 +19,18 @@ app.get('/', function(request, response) {
 });
 
 app.get('/components', function(request, response) {
-	var links = [networkHeader.getLink(), networkFooter.getLink()];
+	var links = _.map(components, function(component) {
+        return component.getLink();
+    });
+
 	response.setHeader('Content-Type', 'application/json');
 	response.end(JSON.stringify(links));
 });
+
+_.each(components, function(component) {
+    component.addRoutes(app);
+});
+
 
 app.listen(3000);
 console.log("dude, im totally listening on port 3000");
