f3dc4e72e92550d86808373dd0519f6bcd0073ae
Cam Swords <cam.swords@gmail.com>
2013-04-18 17:41:07 +1000

    removed a redundant route in the footer api

--- microservice/lib/network_footer_handlers.js
+++ microservice/lib/network_footer_handlers.js
@@ -6,9 +6,6 @@ exports.getLink = function() {
 };
 
 exports.addRoutes = function(app) {
-    app.get('/component/networkHeader', function(request, response) {
-        response_helpers.return_links([{ rel: 'footer', href: 'http://localhost:3000/component/networkFooter', type: 'text/html' }]);
-    });
 
     app.get('/component/networkFooter', function(request, response) {
         response_helpers.stream_file('./html/dark_header.html', response);
