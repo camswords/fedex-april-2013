0bfa1be3a2d8a3bb8a3a9147fab5a703568fc161
Cam Swords <cam.swords@gmail.com>
2013-04-18 15:16:39 +1000

    ensure that the request ends!

--- microservice/server.js
+++ microservice/server.js
@@ -6,15 +6,25 @@ var app = express();
 app.get('/', function(request, response) {
 	var links = [{ rel: 'components', href: 'http://localhost:3000/components', type: 'application/json' }];
 	response.setHeader('Content-Type', 'application/json');
-	response.write(JSON.stringify(links));
+	response.end(JSON.stringify(links));
 });
 
 app.get('/components', function(request, response) {
 	var links = [{ rel: 'networkHeader', href: 'http://localhost:3000/component/networkHeader', type: 'application/json' }];
 	response.setHeader('Content-Type', 'application/json');
-	response.write(JSON.stringify(links));
+	response.end(JSON.stringify(links));
 });
 
+app.get('/component/networkHeader', function(request, response) {
+	var links = [{ rel: 'light', href: 'http://localhost:3000/component/networkHeader/light', type: 'text/html' }];
+	response.setHeader('Content-Type', 'application/json');
+	response.end(JSON.stringify(links));
+});
+
+app.get('/component/networkHeader/light', function(request, response) {
+	response.setHeader('Content-Type', 'text/html');
+	response.end("<html><body>The network header!</body></html>");
+});
 
 app.listen(3000);
 console.log("dude, im totally listening on port 3000");
