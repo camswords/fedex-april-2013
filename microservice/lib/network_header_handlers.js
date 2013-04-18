93f0d3db4a1b58bc10682b4ceb14c683a104abaf
Cam Swords <cam.swords@gmail.com>
2013-04-18 17:10:45 +1000

    the dark header is also available

--- microservice/lib/network_header_handlers.js
+++ microservice/lib/network_header_handlers.js
@@ -2,6 +2,19 @@
 var path = require('path');
 var fs = require('fs');
 
+var stream_file = function(filename, response) {
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            response.writeHead(404, 'Not Found');
+            response.write('<html><body><p>404 Not Found</p><p>Failed to find file ' + filename + '</p></body></html>');
+            response.end();
+        }
+
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(response);
+    });
+};
+
 exports.addMiddleware = function(app) {
 };
 
@@ -11,23 +24,18 @@ exports.getLink = function() {
 
 exports.addRoutes = function(app) {
     app.get('/component/networkHeader', function(request, response) {
-        var links = [{ rel: 'light', href: 'http://localhost:3000/component/networkHeader/light', type: 'text/html' }];
+        var light_header = { rel: 'light', href: 'http://localhost:3000/component/networkHeader/light', type: 'text/html' };
+        var dark_header = { rel: 'dark', href: 'http://localhost:3000/component/networkHeader/dark', type: 'text/html' };
+        var links = [light_header, dark_header];
         response.setHeader('Content-Type', 'application/json');
         response.end(JSON.stringify(links));
     });
 
     app.get('/component/networkHeader/light', function(request, response) {
-        var filename = path.join(process.cwd(), './html/womansday.ninemsn.com.au.html');
+        stream_file(path.join(process.cwd(), './html/light_header.html'), response);
+    });
 
-        fs.exists(filename, function(exists) {
-            if(!exists) {
-                response.writeHead(404, 'Not Found');
-                response.write('<html><body><p>404 Not Found</p><p>Failed to find file ' + filename + '</p></body></html>');
-                response.end();
-            }
-
-            var fileStream = fs.createReadStream('./html/womansday.ninemsn.com.au.html');
-            fileStream.pipe(response);
-        });
+    app.get('/component/networkHeader/dark', function(request, response) {
+        stream_file(path.join(process.cwd(), './html/dark_header.html'), response);
     });
 };
